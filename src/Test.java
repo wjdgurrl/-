ecce1c989f10a746dba798c63d5f326346807f95
mac <gurrl8356@gmail.com>
2022-01-18 19:26:09 +0900

    2021/01/18

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+class Printer
+{
+    int numOfpage;
+    void print(int amount)
+    {
+        System.out.println();
+    }
+}
+public class PrinterTest
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
